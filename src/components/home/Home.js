9e96015ab87388da0e7a6d997bd86e186e518a93
motasemZaidAlkilani <m.kilany1999@gmail.com>
2022-02-15 16:01:37 +0200

    see more feature

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,13 +4,10 @@ import Tours_details from "../TourDetails/TourDetails";
 import Footer from "../footer/Footer";
 import data from "../data/db.json";
 import {Routes,Route} from "react-router-dom";
-import {useState} from "react";
 function Home(){
-    const [isText,setText]=useState(false);
+    
 
-    const handleText=()=>{
-        setText(!isText);
-    }
+   
 return (
     <>
     {}
@@ -20,7 +17,7 @@ return (
     {}
     
     <Route path="/" element={<Tours data={data}/>}/>
-    <Route path={`/city/:id`} element={<Tours_details data={data} isText={isText} handleText={handleText}/>}/>
+    <Route path="/city/:id" element={<Tours_details data={data}  />}/>
     {}
   
     </Routes>
